65e605142225101d81cec66a0acc7dd9ea46a771
Sarah Henney <shenney@callibrity.com>
2019-04-09 12:11:37 -0400

    Some cleanup

--- Crossroads.Service.Auth.IntegrationTest/cypress/integration/malformed_request_spec.js
+++ Crossroads.Service.Auth.IntegrationTest/cypress/integration/malformed_request_spec.js
@@ -6,14 +6,12 @@ describe('Tests malformed requests', function () {
     it(`Request cannot be decoded given token '${token}'`, function () {
       cy.request({
         method: 'GET',
-        url: `${Cypress.config().baseUrl}/api/authorize`,
+        url: '/api/authorize',
         headers: { Authorization: token },
         failOnStatusCode: false
       }).then(response => {
         expect(response.body).to.contain(malformedResponse);
         expect(response.status).to.eq(400);
-        //Want the call to 'fail' if it takes too long to respond? We can do this!
-        expect(response.duration).to.be.lte(1000);
       });
     });
   });
@@ -21,29 +19,25 @@ describe('Tests malformed requests', function () {
   it('Request cannot be decoded when missing headers', function () {
     cy.request({
       method: 'GET',
-      url: `${Cypress.config().baseUrl}/api/authorize`,
+      url: '/api/authorize',
       failOnStatusCode: false
     }).then(response => {
       expect(response.body).to.contain(malformedResponse);
       expect(response.status).to.eq(400);
-      //Want the call to 'fail' if it takes too long to respond? We can do this!
-      expect(response.duration).to.be.lte(1000);
     });
   });
 
   it('Request cannot be decoded when headers have misspelled keys', function () {
-    const expiredMPToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ijkyc3c1bmhtbjBQS3N0T0k1YS1nVVZlUC1NWSIsImtpZCI6Ijkyc3c1bmhtbjBQS3N0T0k1YS1nVVZlUC1NWSJ9.eyJpc3MiOiJGb3JtcyIsImF1ZCI6IkZvcm1zL3Jlc291cmNlcyIsImV4cCI6MTU0MTAxMDEzNSwibmJmIjoxNTQxMDA4MzM1LCJjbGllbnRfaWQiOiJDUkRTLkNvbW1vbiIsInNjb3BlIjpbIm9wZW5pZCIsIm9mZmxpbmVfYWNjZXNzIiwiaHR0cDovL3d3dy50aGlua21pbmlzdHJ5LmNvbS9kYXRhcGxhdGZvcm0vc2NvcGVzL2FsbCJdLCJzdWIiOiJiZDg4ZTk3Mi00ZDEwLTRmNzAtOGM3Zi04ZTUzZTg5YTAwNDgiLCJhdXRoX3RpbWUiOjE1Mzk5NTE4NDEsImlkcCI6Imlkc3J2IiwibmFtZSI6ImRjb3VydHNAY2FsbGlicml0eS5jb20iLCJhbXIiOlsicGFzc3dvcmQiXX0.yx4INlKhWf63zrt0qJtWzfqsHDGQ-LBXo7Outzy9nLCWyw3vHbtRTv4RzjMMg9GkRhdD0woMXhkfp4j9WKw_fJLlZU_A_T9P0NAvXJWMhsZnQwZzTn-xb6_WSqp7QzGwj8JAMXgd1-fvuwA6bflHwrGtEQYr3SMpq8nhHq9KTBCHOMY4-8tKZ1Me0mgCdemeMeYyjlDkuUFJHxWy1L7l4ZhFc1GFxZK5bbzRlqmMcpLSuMw2fb8UVBsRgcWfV3-xLDTqlTfrKLyBKBX5FUplxZqQic-c9d6PpgN_axR5Nss504nNgtPx32eBch-S-_RupM9hdY62ml85UxPgOSL1LA';
+    const expiredOktaToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ijkyc3c1bmhtbjBQS3N0T0k1YS1nVVZlUC1NWSIsImtpZCI6Ijkyc3c1bmhtbjBQS3N0T0k1YS1nVVZlUC1NWSJ9.eyJpc3MiOiJGb3JtcyIsImF1ZCI6IkZvcm1zL3Jlc291cmNlcyIsImV4cCI6MTU1NDc0OTMwNiwibmJmIjoxNTU0NzQ3NTA2LCJjbGllbnRfaWQiOiJDUkRTLkNvbW1vbiIsInNjb3BlIjpbImh0dHA6Ly93d3cudGhpbmttaW5pc3RyeS5jb20vZGF0YXBsYXRmb3JtL3Njb3Blcy9hbGwiLCJvZmZsaW5lX2FjY2VzcyIsIm9wZW5pZCJdLCJzdWIiOiI4YTI5MDA5Mi0zNDU5LTRmNWEtOTc3ZS01Y2M3YjAyZTczNDIiLCJhdXRoX3RpbWUiOjE1NTQ3NDc1MDYsImlkcCI6Imlkc3J2IiwibmFtZSI6Im1wY3JkcythdXRvKzJAZ21haWwuY29tIiwiYW1yIjpbInBhc3N3b3JkIl19.PJuvVth43CIkH1UEgM9n2QXqW91dXwJxn19W_QKQqMbUGvj7d5MVL3aBRfD7O_sZzW3mbZM3LkNBN1caVVVvizi88mqwwf2ywGHWqS-fhDxBoTyhMJ0emm5ZfWdpDHMTh5r0bG3jYys0voY1sNVu1F3QY9_BXFyorHdwS178juHjtNe6hhRNoVJG6Mg82NMr_piaqCCvNIiehlMlpf4B458JDUanbp63PiCHZhWK3Y4LIPpg1MQ7xadI14KMnEgIMoy4nLapK6-dv52Oe-9Qz-sejbQeaUAHRhUDJjkFwZ0xE8jyLGwzXp35uWROXjt8HnbsTobvwKoat7rDVBGdIg';
 
     cy.request({
       method: 'GET',
-      url: `${Cypress.config().baseUrl}/api/authorize`,
-      headers: { authorisation: expiredMPToken },
+      url: '/api/authorize',
+      headers: { authorisation: expiredOktaToken },
       failOnStatusCode: false
     }).then(response => {
       expect(response.body).to.contain(malformedResponse);
       expect(response.status).to.eq(400);
-      //Want the call to 'fail' if it takes too long to respond? We can do this!
-      expect(response.duration).to.be.lte(1000);
     });
   });
 });
