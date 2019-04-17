84748287981fae1f6cd6d588f3690975be25620e
Sarah Henney <shenney@callibrity.com>
2019-04-17 15:25:53 -0400

    Fixed VaultVariables class

--- Crossroads.Service.Auth.IntegrationTest/cypress/plugins/index.js
+++ Crossroads.Service.Auth.IntegrationTest/cypress/plugins/index.js
@@ -1,142 +1,76 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-//TODO working as is, but try to find a way to store the VaultToken for use later
-const https = require('https');
-// function getVaultToken(config) {
-//   return new Promise(function (resolve, reject) {
-//     var options = {
-//       'method': 'POST',
-//       'hostname': 'vault.crossroads.net',
-//       'path': '/v1/auth/approle/login',
-//       'headers': {
-//         'cache-control': 'no-cache'
-//       }
-//     };
-
-//     var req = https.request(options, function (res) {
-//       var chunks = [];
-
-//       res.on('data', function (chunk) {
-//         chunks.push(chunk);
-//       });
-
-//       res.on('end', function () {
-//         var body = Buffer.concat(chunks);
-//         const clientToken = JSON.parse(body).auth.client_token;
-//         resolve(clientToken);
-//       });
-//     }).on('error', (e) => {
-//       reject(Error(`Something went wrong retreiving the client token from Okta:\n${e}`));
-//     });
-
-//     req.write(`{\n"role_id": "${config.env.VAULT_ROLE}",\n"secret_id": "${config.env.VAULT_SECRET}"\n}`);
-//     req.end();
-//   });
-// }
-
-// function getVaultSecrets(vaultUrl, vaultToken) {
-//   const pathComponents = vaultUrl.match(RegExp('https?://([\\w|.]+)(/.*)'));
-//   const hostname = pathComponents[1];
-//   const path = pathComponents[2];
-
-//   return new Promise(function (resolve, reject) {
-//     var options = {
-//       'hostname': hostname,
-//       'path': path,
-//       'headers': {
-//         'cache-control': 'no-cache',
-//         'x-vault-token': vaultToken,
-//       }
-//     };
-
-//     var req = https.get(options, function (res) {
-//       var chunks = [];
-
-//       res.on('data', function (chunk) {
-//         chunks.push(chunk);
-//       });
-
-//       res.on('end', function () {
-//         var body = Buffer.concat(chunks);
-//         const variables = JSON.parse(body).data.data;
-//         resolve(variables);
-//       });
-//     }).on('error', (e) => {
-//       reject(Error(`Something went wrong requesting a secret from Okta:\n${e}`));
-//     });
-
-//     req.end();
-//   });
-// }
-
-// //Returns modified config file or error message
-// function addVariablesToConfig(config, vaultToken, vaultUrl, varsToStore) {
-//   return new Promise(function (resolve, reject) {
-//     getVaultSecrets(vaultUrl, vaultToken).then(vaultVariables => {
-//       varsToStore.forEach(varName => {
-//         config.env[varName] = vaultVariables[varName];
-//       });
-//       resolve(config);
-//     }, error => {
-//       reject(error);
-//     });
-//   });
-// }
-
-//for each entry in VAULT_VAR_PATHS, addVariablesToConfig
-function addAllVariablesToConfig(config) {
-  const vvm = new VaultVariables(config.env.VAULT_ROLE, config.env.VAULT_SECRET);
-
-  return vvm.getVaultToken().then(vaultToken => {
-    return vvm.addVariablesToConfigBulk(vaultToken, config.env.VAULT_VAR_PATHS).then(() => {
-      return vvm.config;
-    })
-    // const vaultPathList = Object.keys(config.env.VAULT_VAR_PATHS);
-    // return Promise.all(vaultPathList.map(path => {
-    //   const varsToStore = config.env.VAULT_VAR_PATHS[path];
-    //   return vvm.addVariablesToConfig(vaultToken, path, varsToStore);
-    // })).then(() => {
-    //   console.log(`after Promise.all, VV config is ${Object.keys(vvm.config)}`);
-    //   return vvm.config;
-    // }, error => {
-    //   throw error;
-    // });
-  }, error => {
-    throw error;
-  });
-}
-
 module.exports = (on, config) => {
-  return addAllVariablesToConfig(config).then(newConfig => {
-    config.env = newConfig;
-    // console.log(`In the end, new config env is ${Object.keys(config.env)}`);
+  const vaultVariables = new VaultVariables(config.env.VAULT_ROLE, config.env.VAULT_SECRET);
+
+  return vaultVariables.getVariablesFromManyVaults(config.env.VAULT_VAR_URLS).then(vaultConfig => {
+    config.env = vaultConfig;
     return config;
   });
 };
 
+const https = require('https');
 class VaultVariables {
   constructor (vaultRole, vaultSecret) {
-    this._vault_components = {vault_token: undefined};//TODO add role and secret?
-
     this._vault_role = vaultRole;
     this._vault_secret = vaultSecret;
-    this._config_object = {};
+    this._variable_config = {};
   }
 
-  getVaultToken() {
-    const vaultRole = this._vault_role; //TODO can these be "this"?
+  get config() {
+    return this._variable_config;
+  }
+
+  /**
+   * Fetches and stores each variable from Vault in the class's config object.
+   * @param {String} vaultURL - fully qualified Vault url
+   * @param {String | Object[]} vaultVariableNames - a single variable name OR an array of variable names to retrieve from Vault
+   * @returns {Promise} the updated config object OR an error
+   * @example
+   * getVariablesFromOneVault("https://vault.crossroads.net/env/staging/testVariables", "SECRET_VARIABLE");
+   * getVariablesFromOneVault("https://vault.crossroads.net/env/staging/testUsers", ["TEST_USER_1_ID", "TEST_USER_1_PW"]);
+   */
+  getVariablesFromOneVault(vaultURL, vaultVariableNames) {
+    if (!Array.isArray(vaultVariableNames)) {
+      vaultVariableNames = [vaultVariableNames];
+    }
+
+    return this._getVaultToken().then(vaultToken => {
+      return this._fetchAndAddVariablesToConfig(vaultToken, vaultURL, vaultVariableNames);
+    });
+  }
+
+  /**
+   * Fetches and stores each variable from each Vault url in the class's config object.
+   * @param {Object} vaultURLsAndVariables - an object where properties are Vault URLs with an array of Vault variable names assigned them.
+   * @returns {Promise} the updated config object OR an error
+   * @example
+   * getVariablesFromManyVaults (
+   * { "https://vault.crossroads.net/env/staging/testUsers": ["TEST_USER_1_ID", "TEST_USER_1_PW"],
+   *   "https://vault.crossroads.net/env/staging/testVariables": ["SECRET_VARIABLE"] });
+   */
+  getVariablesFromManyVaults(vaultURLsAndVariables) {
+    const vaultURLs = Object.keys(vaultURLsAndVariables);
+    const config = this._variable_config;
+
+    return this._getVaultToken().then(vaultToken => {
+      const storeVarsPromiseArray = vaultURLs.map(url => {
+        const variableNames = vaultURLsAndVariables[url];
+        return this._fetchAndAddVariablesToConfig(vaultToken, url, variableNames);
+      });
+
+      return Promise.all(storeVarsPromiseArray).then(() => {
+        return config;
+      });
+    });
+  }
+
+  /**
+   * Fetches the x-vault-token needed to query Vault for secrets.
+   * @returns {Promise} the token's value OR an error
+   */
+  _getVaultToken() {
+    const vaultRole = this._vault_role;
     const vaultSecret = this._vault_secret;
+
     return new Promise(function (resolve, reject) {
       var options = {
         'method': 'POST',
@@ -144,7 +78,8 @@ class VaultVariables {
         'path': '/v1/auth/approle/login',
         'headers': {
           'cache-control': 'no-cache'
-        }
+        },
+        'timeout': 5000
       };
 
       var req = https.request(options, function (res) {
@@ -156,11 +91,17 @@ class VaultVariables {
 
         res.on('end', function () {
           var body = Buffer.concat(chunks);
-          const clientToken = JSON.parse(body).auth.client_token;
-          resolve(clientToken);
+
+          if (res.statusCode === 200) {
+            const vaultToken = JSON.parse(body).auth.client_token;
+            resolve(vaultToken);
+          }
+          else {
+            reject(Error(`Requesting the Vault token failed with status code ${res.statusCode}. Response was:\n${body}`));
+          }
         });
       }).on('error', (e) => {
-        reject(Error(`Something went wrong retreiving the client token from Okta:\n${e}`));
+        reject(Error(`Something went wrong retreiving the Vault token:\n${e}`));
       });
 
       req.write(`{\n"role_id": "${vaultRole}",\n"secret_id": "${vaultSecret}"\n}`);
@@ -168,33 +109,56 @@ class VaultVariables {
     });
   }
 
-  //If vault token has been retreived, will use existing
-  // _storeVaultToken() {
-  //   const vaultComponents = this._vault_components;
-  //   const getVaultToken = this.getVaultToken;
-  //   return new Promise(function (resolve, reject) {
-  //     if (vaultComponents.vault_token === undefined) {
-  //       getVaultToken().then(token => {
-  //         vaultComponents.vault_token = token;
-  //         resolve();
-  //       }, error => {
-  //         reject(error);
-  //       });
-  //     }
-  //     else {
-  //       resolve();
-  //     }
-  //   });
-  // }
+  /**
+   * Helper function that fetches the desired variables from Vault and stores them in the class's config object.
+   * @param {String} vaultToken - x-vault-token
+   * @param {String} vaultUrl - fully qualified Vault url
+   * @param {Object[]} vaultVariableNames - array of variables to retrieve from Vault
+   * @returns {Promise} the updated config object OR an error
+   */
+  _fetchAndAddVariablesToConfig(vaultToken, vaultUrl, vaultVariableNames) {
+    const config = this._variable_config;
+    const getVaultSecrets = this._getVaultSecrets;
 
-  _storeVaultToken(vaultToken){
-    this._vault_token = vaultToken;
+    if (!Array.isArray(vaultVariableNames)) {
+      throw Error(`Expected an array, but given ${vaultVariableNames}.`);
+    }
+
+    return new Promise(function (resolve, reject) {
+      getVaultSecrets(vaultToken, vaultUrl).then(vaultVariables => {
+        vaultVariableNames.forEach(varName => {
+          config[varName] = vaultVariables[varName];
+
+          if (vaultVariables[varName] === undefined) {
+            reject(Error(`${varName} could not be found in ${vaultUrl}`));
+          }
+        });
+
+        resolve(config);
+      }, error => {
+        reject(error);
+      });
+    });
   }
 
-  getVaultSecrets(vaultUrl, vaultToken) {
-    const pathComponents = vaultUrl.match(RegExp('https?://([\\w|.]+)(/.*)'));
-    const hostname = pathComponents[1];
-    const path = pathComponents[2];
+  /**
+   * Fetches the secrets stored at the given Vault url.
+   * @param {String} vaultToken - x-vault-token
+   * @param {String} vaultUrl - fully qualified Vault url
+   * @returns {Promise} an object with the secret's keys and their values OR an error
+   */
+  _getVaultSecrets(vaultToken, vaultUrl) {
+    const urlPattern = 'https?://([\\w|.]+)(/.*)';
+    let hostname;
+    let path;
+
+    try {
+      const pathComponents = vaultUrl.match(RegExp(urlPattern));
+      hostname = pathComponents[1];
+      path = pathComponents[2];
+    } catch (e) {
+      throw Error(`Vault url "${vaultUrl}" did not match pattern "${urlPattern}".\n${e}`);
+    }
 
     return new Promise(function (resolve, reject) {
       var options = {
@@ -203,7 +167,8 @@ class VaultVariables {
         'headers': {
           'cache-control': 'no-cache',
           'x-vault-token': vaultToken,
-        }
+        },
+        'timeout': 5000
       };
 
       var req = https.get(options, function (res) {
@@ -215,74 +180,20 @@ class VaultVariables {
 
         res.on('end', function () {
           var body = Buffer.concat(chunks);
-          const variables = JSON.parse(body).data.data;
-          resolve(variables);
+
+          if (res.statusCode === 200) {
+            const variables = JSON.parse(body).data.data;
+            resolve(variables);
+          }
+          else {
+            reject(Error(`Requesting a Vault secret failed with status code ${res.statusCode}. Response was:\n${body}`));
+          }
         });
       }).on('error', (e) => {
-        reject(Error(`Something went wrong requesting a secret from Okta:\n${e}`));
+        reject(new Error(`Something went wrong requesting a Vault secret:\n${e}`));
       });
 
       req.end();
     });
-  }
-
-  addVariablesToConfigWORKING(vaultToken, vaultUrl, varsToStore) {
-    const configObj = this._config_object;
-    const getVaultSecrets = this.getVaultSecrets;
-    return new Promise(function (resolve, reject) {
-      getVaultSecrets(vaultUrl, vaultToken).then(vaultVariables => {
-        varsToStore.forEach(varName => {
-          configObj[varName] = vaultVariables[varName];
-        });
-        resolve();
-      }, error => {
-        reject(error);
-      });
-    });
-  }
-
-  //returns the config - do we want/need this?
-  addVariablesToConfig(vaultToken, vaultUrl, varsToStore) {
-    const configObj = this._config_object;
-    const getVaultToken = this._storeVaultToken;
-    const getVaultSecrets = this.getVaultSecrets;
-    const vaultComponents = this._vault_components;
-
-    return new Promise(function (resolve, reject) {
-      getVaultSecrets(vaultUrl, vaultToken).then(vaultVariables => {
-        varsToStore.forEach(varName => {
-          configObj[varName] = vaultVariables[varName];
-        });
-        resolve();
-      }, error => {
-        reject(error);
-      });
-    });
-  }
-
-  /**
-  * Given an object where properties are vault urls and their values are an array of secrets
-  * to retreive from that vault.
-  * @example
-  * { "https://vault.crossroads.net/env/staging/testUsers": ["TEST_USER_1_ID", "TEST_USER_1_PW"] }
-  **/
-  addVariablesToConfigBulk(vaultToken, pathWithVariables) {
-    const vaultPathList = Object.keys(pathWithVariables);
-
-    const promiseArray = vaultPathList.map((path) => {
-      const varsToStore = pathWithVariables[path];
-      return this.addVariablesToConfig(vaultToken, path, varsToStore);
-    });
-
-    return Promise.all(promiseArray);
-    // .then(() => {
-    //   return vvm.config;
-    // }, error => {
-    //   throw error;
-    // });
-  }
-
-  get config() {
-    return this._config_object;
   }
 }
